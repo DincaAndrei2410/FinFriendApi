e4b72eb923c2583a24aa3768b8995232b3f5d4c2
botezatubianca97 <botezatubianca97@gmail.com>
2021-02-20 21:54:35 +0200

    chart endpoint

--- controllers/finance.js
+++ controllers/finance.js
@@ -1,56 +1,76 @@
 const express = require('express')
 const router = express.Router()
 const yahooFinance = require('yahoo-finance');
+var unirest = require("unirest");
 
 const SYMBOLS = [
-    'AAPL',
-    'AMZN',
-    'GOOGL',
-    'TSLA',
-    'FB',
-    'NFLX'
-  ];
-  
+  'AAPL',
+  'AMZN',
+  'GOOGL',
+  'TSLA',
+  'FB',
+  'NFLX'
+];
+
 router.get('/historical', (req, res) => {
-    yahooFinance.historical({
-        symbols: SYMBOLS,
-        from: '2021-01-01',
-        to: '2021-02-20',
-        period: 'd'
-      }).then(function (result) {
-        res.status(200).send(result);
-      }).catch((err) => {
-        res.status(500).send(err)
-      })
+  yahooFinance.historical({
+    symbols: SYMBOLS,
+    from: '2021-01-01',
+    to: '2021-02-20',
+    period: 'd'
+  }).then(function (result) {
+    res.status(200).send(result);
+  }).catch((err) => {
+    res.status(500).send(err)
+  })
 });
 
 router.get('/historicalByCompany/:symbol', (req, res) => {
   const symbol = [req.params.symbol];
   yahooFinance.historical({
-      symbols: symbol,
-      from: '2021-01-01',
-      to: '2021-02-20',
-      period: 'd'
-    }).then(function (result) {
-      res.status(200).send(result);
-    }).catch((err) => {
-      res.status(500).send(err)
-    })
+    symbols: symbol,
+    from: '2021-01-01',
+    to: '2021-02-20',
+    period: 'd'
+  }).then(function (result) {
+    res.status(200).send(result);
+  }).catch((err) => {
+    res.status(500).send(err)
+  })
 });
 
 router.get('/summarybyCompany/:symbol', (req, res) => {
   const symbol = req.params.symbol;
   yahooFinance.quote({
-      symbol: symbol,
-      modules: [ 'price', 'summaryDetail', 'financialData' ] 
-    }).then(function (result) {
-      res.status(200).send(result);
-    }).catch((err) => {
-      res.status(500).send(err)
-    })
+    symbol: symbol,
+    modules: ['price', 'summaryDetail', 'financialData']
+  }).then(function (result) {
+    res.status(200).send(result);
+  }).catch((err) => {
+    res.status(500).send(err)
+  })
 });
 
+router.get('/chart/:symbol', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  const symbol = req.params.symbol;
+  var req = unirest("GET", "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-chart");
+  req.query({
+    "interval": "5m",
+    "symbol": symbol,
+    "range": "5d",
+  });
+  req.headers({
+    "x-rapidapi-key": "e499b14d1amshcf40a6f5bde46dep1721f4jsn0a021671d1dd",
+    "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+    "useQueryString": true
+  });
 
+  req.end(function (resp) {
+    if (resp.error) throw new Error(resp.error);
+    res.send(resp.body);
+  });
+});
 
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const http = require('http').Server(app);
 
 const simulators = require('./controllers/simulators.js');
 const finance = require('./controllers/finance.js');
-
+app.options('*', cors()) // include before other routes 
+app.use(cors({
+  origin: ['http://localhost:8080'],
+  credentials: true,
+}));
+app.use(function (err, req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With, Content-Type, Accept, Authorization');
+})
 app.use(bodyParser.json());
 app.use(bodyParser.json({
   limit: '50mb'
