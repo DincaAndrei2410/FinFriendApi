c141d2cbab75c310f8d09fcf14bcd3de2f37a12f
botezatubianca97 <botezatubianca97@gmail.com>
2021-02-27 14:00:58 +0200

    https

--- index.js
+++ index.js
@@ -3,38 +3,56 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const http = require('http').Server(app);
+const https = require('https');
+const fs = require('fs')
 
 const simulators = require('./controllers/simulators.js');
 const finance = require('./controllers/finance.js');
 const sentiment = require('./controllers/sentiment.js');
 
-app.options('*', cors())
+const port = 3005;
+const portSecure = 3006;
+
+app.options('*', cors());
+
 app.use(cors({
   origin: ['http://localhost:8080'],
   credentials: true,
 }));
+
 app.use(function (err, req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With, Content-Type, Accept, Authorization');
 })
+
 app.use(bodyParser.json());
+
 app.use(bodyParser.json({
   limit: '50mb'
 }));
+
 app.use(bodyParser.urlencoded({
   limit: '50mb',
   extended: true
 }));
+
+const server = https.createServer({
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+  requestCert: false,
+  rejectUnauthorized: false
+}, app).listen(portSecure, () => {
+  console.log(`Server started at port: ${portSecure} for https`)
+})
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-const port = 3005;
-http.listen(port, () => console.log(`Server started at port: ${port}`));
+// http.listen(port, () => console.log(`Server started at port: ${port}`));
 
 app.use('/simulators', simulators);
 app.use('/finance', finance);
